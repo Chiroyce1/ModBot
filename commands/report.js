cd7b77988536822d620c7a7c75f9529f3708354c
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-08-21 21:30:26 +0530

    Embed v1

--- commands/report.js
+++ commands/report.js
@@ -1,4 +1,4 @@
-import { ContextMenuCommandBuilder, ApplicationCommandType } from "discord.js";
+import { ContextMenuCommandBuilder, ApplicationCommandType, EmbedBuilder } from "discord.js";
 
 const MOD_CHANNEL = "1010854019733917716";
 const url_format = "https://discord.com/channels/1010852997858852964/{CHANNEL}/{ID}";
@@ -8,8 +8,16 @@ export const data = new ContextMenuCommandBuilder()
     .setType(ApplicationCommandType.Message)
 
 export const execute = async (interaction, client) => {
-    interaction.reply({ content: `Message successfully reported to moderators.`, ephemeral: true });
-    console.log(interaction);
     const channel = client.channels.cache.get(MOD_CHANNEL);
-    channel.send(`<@${interaction.user.id}> reported ${url_format.replaceAll("{CHANNEL}", interaction.channelId).replaceAll("{ID}", interaction.targetId)}`);
+    const embedDesc = `**Reporter**: <@${interaction.user.id}>
+[**Jump to message**](${url_format.replaceAll("{CHANNEL}", interaction.channelId).replaceAll("{ID}", interaction.targetId)})`
+
+    const embed = new EmbedBuilder()
+        .setTitle(`Message reported by ${interaction.user.username}#${interaction.user.discriminator}`)
+        .setDescription(embedDesc)
+        .setColor(0xFF0000)
+        .setTimestamp()
+
+    channel.send({ embeds: [embed] });
+    interaction.reply({ content: `Message successfully reported to moderators.`, ephemeral: true });
 }
