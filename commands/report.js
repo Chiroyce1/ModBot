c1be14a9392164d63e25961950faa185ac47fbe5
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-08-22 19:35:41 +0530

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,14 +1,13 @@
 import { ContextMenuCommandBuilder, ApplicationCommandType, EmbedBuilder } from "discord.js";
 
-const MOD_CHANNEL = "1010854019733917716";
-const url_format = "https://discord.com/channels/1010852997858852964/{CHANNEL}/{ID}";
+const url_format = `https://discord.com/channels/${process.env.GUILD_ID}/{CHANNEL}/{ID}`;
 
 export const data = new ContextMenuCommandBuilder()
     .setName('Report')
     .setType(ApplicationCommandType.Message)
 
 export const execute = async (interaction, client) => {
-    const channel = client.channels.cache.get(MOD_CHANNEL);
+    const channel = client.channels.cache.get(process.env.MOD_CHANNEL);
     const embedDesc = `**Reporter**: <@${interaction.user.id}>
 [**Jump to message**](${url_format.replaceAll("{CHANNEL}", interaction.channelId).replaceAll("{ID}", interaction.targetId)})`
 
